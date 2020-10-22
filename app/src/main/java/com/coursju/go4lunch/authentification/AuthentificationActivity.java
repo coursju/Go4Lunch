b67d257f4bc1c04b08b985fdaa4267435fb128a5
coursju <coursju@outlook.com>
2020-10-22 17:22:09 +0400

    activity_authentification.xml added

---
+++ app/src/main/java/com/coursju/go4lunch/authentification/AuthentificationActivity.java
@@ -0,0 +1,16 @@
+package com.coursju.go4lunch.authentification;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.coursju.go4lunch.R;
+
+public class AuthentificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentification);
+    }
+}
